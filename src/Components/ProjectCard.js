9068b0e7b34e45ee7d112c07e6fd790e62271432
MarcotulliMatteo <marcotullimatteo5b@gmail.com>
2022-10-18 17:52:35 +0200

    feat: carousel projects

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,15 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { TextH4, BorderBottomAppearH3 } from "../StyledComponents/StyledComponents";
 
 const ProjectCard = (props) => {
     const [mouseOver, setMouseOver] = useState(false)
 
+    const [renderImage, setRenderImage] = useState({
+        img: props.image[0],
+        index: 0
+    })
+
+    useEffect(() => {
+        if(props.image.length <= 1) return
+        const interval = setInterval(() => {
+            const index = props.image.length > renderImage.index + 1 ? renderImage.index + 1 : 0;
+            setRenderImage({
+                img: props.image[renderImage.index],
+                index: index
+            })
+        }, 6000)
+        return () => clearInterval(interval)
+    }, [props.image, renderImage])
+
     return (
         <CardDiv color={props.color}>
             <ImageContainerDiv>
                 <Text>{props.title}</Text>
-                <Image src={props.image}/>
+                <Image src={renderImage.img}/>
+                <ProgressDiv>
+                    {
+                        props.image.map((elem, index) => {
+                            return <Circle key={index} color={renderImage.index === index ? 'white': 'grey'}/>
+                        })
+                    }
+                </ProgressDiv>
             </ImageContainerDiv>
             <Overlay opacity={mouseOver ? 0.8 : 0}
              onMouseOver={() => setMouseOver(true)}
@@ -40,7 +64,7 @@ const CardDiv = styled.div`
     padding: 15px;
     align-items: center;
     width: 33vh;
-    height: 200px;
+    height: 230px;
     text-align: center;
     background-color: ${props => props.color};
     justify-content: center;
@@ -64,8 +88,7 @@ const ImageContainerDiv = styled.div`
 
 const Image = styled.img`
     width: 90%;
-    height: 100%;
-    margin-bottom: 20px;
+    height: 65%;
 `
 
 const Text = styled.p`
@@ -76,7 +99,7 @@ const Text = styled.p`
 const Overlay = styled.div`
     padding: 15px; 
     width: 33vh;
-    height: 200px;
+    height: 230px;
     border-radius: 10px;
     opacity: ${props => props.opacity};
     background-color: black;
@@ -91,4 +114,20 @@ const ButtonDiv = styled.div`
     gap: 20px;
     justify-content: center;
     align-items: center;
+`
+
+const Circle = styled.div`
+    border-radius: 50%;
+    width: 7px;
+    height: 7px;
+    background-color: ${props => props.color};
+`
+
+const ProgressDiv = styled.div`
+    width: 100%;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 5px;
 `
--- src/Components/RecentProjects.js
+++ src/Components/RecentProjects.js
@@ -4,8 +4,11 @@ import ProjectCard from "./ProjectCard";
 
 import portfolioProject from "../Images/Portfolio.png";
 import guessTheWordGame from "../Images/GuessTheWordGame.png";
-import billReminder from "../Images/BillReminder.png";
-import animeList from "../Images/AnimeList2.png"
+import billReminder1 from "../Images/BillReminder1.png";
+import billReminder2 from "../Images/BillReminder2.png";
+import billReminder3 from "../Images/BillReminder3.png";
+import animeList2 from "../Images/AnimeList2.png"
+import animeList1 from "../Images/AnimeList1.png"
 
 const RecentProjects = React.forwardRef((props, ref) => {
     return (
@@ -13,20 +16,20 @@ const RecentProjects = React.forwardRef((props, ref) => {
             <ComponentSubContainerDiv>
                 <SectionTitleH2 color={'black'}>My Projects</SectionTitleH2>
                 <CardContainerDiv>
-                    <ProjectCard color='rgb(41,58,80)' image={portfolioProject}
+                    <ProjectCard color='rgb(41,58,80)' image={[portfolioProject]}
                      title='Portfolio' description="This is my Portfolio, written in React"
                      showCode={true} showProject={false} codeHref="https://github.com/MarcotulliMatteo/PersonalPortfolio"/>
 
-                    <ProjectCard color='rgb(68,67,69)' image={guessTheWordGame}
+                    <ProjectCard color='rgb(68,67,69)' image={[guessTheWordGame]}
                      title='Guess The Word' description="This is a Game in which you have to guess a word. The projest is written in React."
                      showCode={true} showProject={true} codeHref="https://github.com/MarcotulliMatteo/GuessTheWordGame"
                      projectHref="https://vermillion-clafoutis-eced69.netlify.app/"/>
 
-                    <ProjectCard color='rgb(88,21,137)' image={billReminder}
+                    <ProjectCard color='rgb(88,21,137)' image={[billReminder1, billReminder2, billReminder3]}
                      title='Bill Reminder' description="Mobile Application for Store all the Bill to be paid and already paid, written in React Native using Firebase and Firebase Authentication."
                      showCode={true} showProject={false} codeHref="https://github.com/MarcotulliMatteo/BillReminder"/>
 
-                    <ProjectCard color='rgb(68,67,69)' image={animeList}
+                    <ProjectCard color='rgb(68,67,69)' image={[animeList1, animeList2]}
                      title='Anime List' description="Social Network about Anime and Manga, written in React Native using Firebase and Firebase Authentication."
                      showCode={true} showProject={false} codeHref="https://github.com/MarcotulliMatteo/AnimeList"/>
                 </CardContainerDiv>
